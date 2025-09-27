7ab2a5aa7bd8dc532beb4d330368e647fb8fd7fc
MocaRt3 <mocart753@gmail.com>
2025-09-27 21:17:38 +0300

    add dishes block

---
+++ js/dishes.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', e =>{
+
+    document.querySelector('.dishes__more').addEventListener('click', ()=>{
+        document.querySelector('.dishes__container--show').classList.toggle('show-translate')
+    })
+
+
+
+
+
+
+
+
+
+});
